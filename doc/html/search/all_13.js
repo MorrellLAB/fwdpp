6aa37da5ed231be90da8632877bea58a0d60461f
Kevin R. Thornton <krthornt@uci.edu>
2015-06-18 06:16:39 -0700

    redoc

--- doc/html/search/all_13.js
+++ doc/html/search/all_13.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['vdipvector_5ft',['vdipvector_t',['../d3/de0/classKTfwd_1_1sugar_1_1metapop.html#a551fcfeedd8a4bde03589ff0be70aa2d',1,'KTfwd::sugar::metapop::vdipvector_t()'],['../d2/d43/classKTfwd_1_1sugar_1_1metapop__serialized.html#a1697e7f8d0bc4fd4b8a1680bd5b26a0f',1,'KTfwd::sugar::metapop_serialized::vdipvector_t()']]],
-  ['vglist_5ft',['vglist_t',['../d3/de0/classKTfwd_1_1sugar_1_1metapop.html#a55a1b50c1fab061aafa687364a688c52',1,'KTfwd::sugar::metapop::vglist_t()'],['../d2/d43/classKTfwd_1_1sugar_1_1metapop__serialized.html#ae0a44a51a6235bfeb6f40537a37f17ac',1,'KTfwd::sugar::metapop_serialized::vglist_t()'],['../d3/d11/structKTfwd_1_1sugar_1_1multiloc.html#afed5c22df54725ab01c6bb4fd80381da',1,'KTfwd::sugar::multiloc::vglist_t()'],['../d0/dc0/structKTfwd_1_1sugar_1_1multiloc__serialized.html#a0a67450b86bb45de12e98d1753dc1f15',1,'KTfwd::sugar::multiloc_serialized::vglist_t()']]],
+  ['vdipvector_5ft',['vdipvector_t',['../d3/de0/classKTfwd_1_1sugar_1_1metapop.html#ab66a65b5c6f2db61470c7d16f6b9181e',1,'KTfwd::sugar::metapop::vdipvector_t()'],['../d2/d43/classKTfwd_1_1sugar_1_1metapop__serialized.html#ab8882a44ca145e6392eb4b764ac6f717',1,'KTfwd::sugar::metapop_serialized::vdipvector_t()']]],
+  ['vglist_5ft',['vglist_t',['../d3/d11/structKTfwd_1_1sugar_1_1multiloc.html#afed5c22df54725ab01c6bb4fd80381da',1,'KTfwd::sugar::multiloc::vglist_t()'],['../d0/dc0/structKTfwd_1_1sugar_1_1multiloc__serialized.html#a0a67450b86bb45de12e98d1753dc1f15',1,'KTfwd::sugar::multiloc_serialized::vglist_t()']]],
   ['void_5ft',['void_t',['../d9/d91/namespaceKTfwd_1_1traits_1_1internal.html#a465b9665a0c6fe06199488fbf1baa4ec',1,'KTfwd::traits::internal']]]
 ];
--- doc/html/search/functions_3.js
+++ doc/html/search/functions_3.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['diploid_5ffitness_5fdispatch',['diploid_fitness_dispatch',['../de/dbd/namespaceKTfwd_1_1fwdpp__internal.html#afc6d610569653768f125848317007328',1,'KTfwd::fwdpp_internal::diploid_fitness_dispatch(const fitness_policy_type &amp;fp, const diploid_itr_t &amp;d, std::false_type)'],['../de/dbd/namespaceKTfwd_1_1fwdpp__internal.html#ac17dccb362b7b7a488ae34ac43bd5738',1,'KTfwd::fwdpp_internal::diploid_fitness_dispatch(const fitness_policy_type &amp;fp, const diploid_itr_t &amp;d, std::true_type)'],['../de/dbd/namespaceKTfwd_1_1fwdpp__internal.html#adbb84152f62a5e3c009467cdbe4b3614',1,'KTfwd::fwdpp_internal::diploid_fitness_dispatch(const fitness_policy_type &amp;fp, const diploid_itr_t &amp;d, std::true_type, typename std::enable_if&lt; traits::is_custom_diploid_t&lt; typename diploid_itr_t::value_type &gt;::type &gt;::type())']]],
-  ['diploid_5ft',['diploid_t',['../dc/dfb/structdiploid__t.html#a4c6a9115136d2d3741d2babbb1cfef6e',1,'diploid_t::diploid_t()'],['../dc/dfb/structdiploid__t.html#adace939f1aafa4ecafe00d7405e2978a',1,'diploid_t::diploid_t(first_type g1, first_type g2)']]],
-  ['duplicate_5fpop',['duplicate_pop',['../d5/dcc/migsel__split__ind__list_8cc.html#a5c950bfe34746777a32b2f61d8ee1351',1,'migsel_split_ind_list.cc']]]
+  ['diploid_5ft',['diploid_t',['../dc/dfb/structdiploid__t.html#a4c6a9115136d2d3741d2babbb1cfef6e',1,'diploid_t::diploid_t()'],['../dc/dfb/structdiploid__t.html#adace939f1aafa4ecafe00d7405e2978a',1,'diploid_t::diploid_t(first_type g1, first_type g2)']]]
 ];
